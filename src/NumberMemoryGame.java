989df285c0cb26f20a8c8e68c6d603c3636f9f21
7DavidMedina7 <60455398+7DavidMedina7@users.noreply.github.com>
2020-04-19 22:09:30 -0600

    Added a life system and finished level 1 of NumberMemoryGame.

--- src/NumberMemoryGame.java
+++ src/NumberMemoryGame.java
@@ -1,9 +1,16 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
-
 public class NumberMemoryGame {
+
+    // Creating a public scanner
+    public static Scanner scan = new Scanner(System.in);
+
+    // Creating a lives variables indicating a player's lives remaining
+    public static int lives = 3;
+
     public static void main(String[] args) {
 
         System.out.println();
@@ -12,7 +19,6 @@ public class NumberMemoryGame {
         System.out.println("*************************************");
 
         System.out.print("\nEnter your name: ");
-        Scanner scan = new Scanner(System.in);
         String userName = scan.next();
 
         clearConsole();
@@ -22,17 +28,21 @@ public class NumberMemoryGame {
         String userResponse = scan.next();
 
         if(userResponse.equalsIgnoreCase("yes")) {
-            System.out.println("\nAlright cool.");
             clearConsole();
-
             System.out.println("\nStarting the game...");
+            try{
+                Thread.sleep(1500);
+            } catch(Exception e) {
+                System.out.print(e);
+            }
 
+            clearConsole();
+            game_level_1();
 
-            game();
         }else if(userResponse.equalsIgnoreCase("no")){
             System.out.println("\nThat's too bad. Goodbye.");
         } else{
-            System.out.println("\nYou think you're funny huh?");
+            System.out.println("\nYou think you're funny, huh?");
             System.out.println("Go waste someone else's life. Lol");
         }
     }
@@ -46,26 +56,93 @@ public class NumberMemoryGame {
         }
     }
 
-    public static void game() {
-        Random rand = new Random();
-        int upperBound = 26;
-
-        // Generating a random number between 0-25
-        int randomNumber = rand.nextInt(upperBound);
-
-        int[] numbers = new int[3];
-
-        // Filling the array with random numbers
-        for(int i = 0; i < numbers.length; i++) {
-            numbers[i] = rand.nextInt(upperBound);
+    // Function to check if the game should be over
+    public static void gameOver() {
+        lives--;
+        if(lives == 0) {
+            System.out.print("\nGame Over");
+            System.exit(0);
         }
+    }
 
-        System.out.println();
-
-        // Printing the array
-        for (int number : numbers) {
-            System.out.print(number + " ");
+    // Function to delay 2.5 seconds
+    public static void time_level_1() {
+        try{
+            Thread.sleep(2500);
+        } catch(Exception e) {
+            System.out.print(e);
         }
-        System.out.print("");
+    }
+
+    // Function to delay 1.5 seconds
+    public static void time_level_2() {
+        try{
+            Thread.sleep(1500);
+        } catch(Exception e) {
+            System.out.print(e);
+        }
+    }
+
+    public static void game_level_1() {
+
+        // Variable indicating when to stop
+        int stop = 10;
+        // Variable to be increased at the end of each iteration
+        int continueGame = 0;
+
+        // Iterating level 1 for 10 times
+        while(continueGame < stop) {
+
+            Random rand = new Random();
+            // Numbers to be generated between 0-99
+            int upperBound = 100;
+
+            int[] numbers = new int[3];
+
+            // Filling the array with random numbers
+            for(int i = 0; i < numbers.length; i++) {
+                numbers[i] = rand.nextInt(upperBound);
+            }
+            System.out.println();
+
+            // Printing the array
+            for (int number : numbers) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+            time_level_1();
+            clearConsole();
+            System.out.println("\nEnter your answer: ");
+
+            // Storing the user's answer to the array
+            int[] answer = new int[3];
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = Integer.parseInt(scan.next());
+            }
+
+            // Comparing the contents of both arrays
+            if (Arrays.equals(numbers, answer)) {
+                System.out.println("\nCorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+            }else {
+                System.out.println("\nIncorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+                gameOver();
+            }
+            // Incrementing the continueGame variable
+            continueGame++;
+        }
     }
 }
