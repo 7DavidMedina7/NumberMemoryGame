ce31672b4fa5679d815cf65d0fe7ddf0014d4997
7DavidMedina7 <60455398+7DavidMedina7@users.noreply.github.com>
2020-05-02 13:55:48 -0600

    Added an 'instructions' section. Added 'level 2' and 'level 3.'

--- src/NumberMemoryGame.java
+++ src/NumberMemoryGame.java
@@ -24,6 +24,14 @@ public class NumberMemoryGame {
         clearConsole();
 
         System.out.println("\nWelcome " + userName + "!\n");
+        System.out.println("*******************************************************************************************");
+        System.out.println("* The purpose of the game is to test your memory capabilities.                            *");
+        System.out.println("* The screen will display a series of numbers for a certain amount of seconds.            *");
+        System.out.println("* Following, you will have to input the numbers exactly as they appeared in the screen    *");
+        System.out.println("* including the spaces. As each level progresses, the complexity of the numbers increases *");
+        System.out.println("* and the time frame by which they appear decreases. The game ends when you answer three  *");
+        System.out.println("* times incorrectly.                                                                      *");
+        System.out.println("*******************************************************************************************\n");
         System.out.print("Ready to take on the challenge? ");
         String userResponse = scan.next();
 
@@ -37,7 +45,33 @@ public class NumberMemoryGame {
             }
 
             clearConsole();
+            System.out.print("~~~LEVEL 1~~~");
+            System.out.print("\nLives remaining: " + lives);
+            displayScreen();
+            clearConsole();
             game_level_1();
+
+            clearConsole();
+            System.out.print("~~~LEVEL 2~~~");
+            System.out.println("\nLives remaining: " + lives);
+            displayScreen();
+            clearConsole();
+            game_level_2();
+
+            clearConsole();
+            System.out.print("~~~LEVEL 3~~~");
+            System.out.println("\nLives remaining: " + lives);
+            displayScreen();
+            clearConsole();
+            game_level_3();
+
+            clearConsole();
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("~      CONGRATULATIONS!     ~");
+            System.out.println("~  YOU COMPLETED THE GAME!  ~");
+            System.out.println("~  YOU ARE NOW A CHAMPION!  ~");
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 
         }else if(userResponse.equalsIgnoreCase("no")){
             System.out.println("\nThat's too bad. Goodbye.");
@@ -66,7 +100,7 @@ public class NumberMemoryGame {
     }
 
     // Function to delay 2.5 seconds
-    public static void time_level_1() {
+    public static void displayScreen() {
         try{
             Thread.sleep(2500);
         } catch(Exception e) {
@@ -74,10 +108,28 @@ public class NumberMemoryGame {
         }
     }
 
-    // Function to delay 1.5 seconds
+    // Function to delay 4.5 seconds
+    public static void time_level_1() {
+        try{
+            Thread.sleep(4500);
+        } catch(Exception e) {
+            System.out.print(e);
+        }
+    }
+
+    // Function to delay 4 seconds
     public static void time_level_2() {
         try{
-            Thread.sleep(1500);
+            Thread.sleep(4000);
+        } catch(Exception e) {
+            System.out.print(e);
+        }
+    }
+
+    // Function to delay 3.5 seconds
+    public static void time_level_3() {
+        try{
+            Thread.sleep(3500);
         } catch(Exception e) {
             System.out.print(e);
         }
@@ -86,11 +138,137 @@ public class NumberMemoryGame {
     public static void game_level_1() {
 
         // Variable indicating when to stop
-        int stop = 10;
+        int stop = 5;
         // Variable to be increased at the end of each iteration
         int continueGame = 0;
 
-        // Iterating level 1 for 10 times
+        // Iterating level 1 for 5 times
+        while(continueGame < stop) {
+
+            Random rand = new Random();
+            // Numbers to be generated between 0-25
+            int upperBound = 26;
+
+            int[] numbers = new int[3];
+
+            // Filling the array with random numbers
+            for(int i = 0; i < numbers.length; i++) {
+                numbers[i] = rand.nextInt(upperBound);
+            }
+            System.out.println();
+
+            // Printing the array
+            for (int number : numbers) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+            time_level_1();
+            clearConsole();
+            System.out.println("\nEnter your answer: ");
+
+            // Storing the user's answer to the array
+            int[] answer = new int[3];
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = Integer.parseInt(scan.next());
+            }
+
+            // Comparing the contents of both arrays
+            if (Arrays.equals(numbers, answer)) {
+                System.out.println("\nCorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+            }else {
+                System.out.println("\nIncorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+                gameOver();
+            }
+            // Incrementing the continueGame variable
+            continueGame++;
+        }
+    }
+
+    public static void game_level_2() {
+
+        // Variable indicating when to stop
+        int stop = 5;
+        // Variable to be increased at the end of each iteration
+        int continueGame = 0;
+
+        // Iterating level 2 for 5 times
+        while(continueGame < stop) {
+
+            Random rand = new Random();
+            // Numbers to be generated between 0-50
+            int upperBound = 51;
+
+            int[] numbers = new int[3];
+
+            // Filling the array with random numbers
+            for(int i = 0; i < numbers.length; i++) {
+                numbers[i] = rand.nextInt(upperBound);
+            }
+            System.out.println();
+
+            // Printing the array
+            for (int number : numbers) {
+                System.out.print(number + " ");
+            }
+            System.out.println();
+            time_level_2();
+            clearConsole();
+            System.out.println("\nEnter your answer: ");
+
+            // Storing the user's answer to the array
+            int[] answer = new int[3];
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = Integer.parseInt(scan.next());
+            }
+
+            // Comparing the contents of both arrays
+            if (Arrays.equals(numbers, answer)) {
+                System.out.println("\nCorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+            }else {
+                System.out.println("\nIncorrect");
+                // Delaying the next console prompt for 1.5 seconds
+                try{
+                    Thread.sleep(1500);
+                } catch(Exception e) {
+                    System.out.print(e);
+                }
+                clearConsole();
+                gameOver();
+            }
+            // Incrementing the continueGame variable
+            continueGame++;
+        }
+    }
+
+    public static void game_level_3() {
+
+        // Variable indicating when to stop
+        int stop = 5;
+        // Variable to be increased at the end of each iteration
+        int continueGame = 0;
+
+        // Iterating level 2 for 5 times
         while(continueGame < stop) {
 
             Random rand = new Random();
@@ -110,7 +288,7 @@ public class NumberMemoryGame {
                 System.out.print(number + " ");
             }
             System.out.println();
-            time_level_1();
+            time_level_3();
             clearConsole();
             System.out.println("\nEnter your answer: ");
 
